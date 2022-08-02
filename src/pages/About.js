0467aec6742e9e3fd2cc07cf3f2dbdea374f7280
stephen azosike <stephenazosike@gmail.com>
2022-08-02 12:00:55 +0000

    commit

---
+++ src/pages/About.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import MultiplePizzas from '../assets/multiplePizzas.jpeg'
+import '../styles/About.css'
+
+function About() {
+    return (
+        <div className='about'>
+            <div className='aboutTop' style={{ backgroundImage: `url(${MultiplePizzas})` }}></div>
+            <div className='aboutBottom'>
+                <h1>About US</h1>
+                <p> lorem ipsum
+                </p>
+            </div>
+        </div>
+    )
+}
+
+export default About
